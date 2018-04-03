693a2985f6bfd83c7ca80608fee199dbe174e704
papush <papush8@gmail.com>
2018-04-03 16:52:31 -0600

    refresh

---
+++ my_script.js
@@ -0,0 +1,12 @@
+$(document).ready( function(){
+$('#auto').load('load.php');
+refresh();
+});
+ 
+function refresh()
+{
+	setTimeout( function() {
+	  $('#auto').fadeOut('slow').load('load.php').fadeIn('slow');
+	  refresh();
+	}, 4000);
+}
